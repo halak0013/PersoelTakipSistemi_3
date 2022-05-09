832445d012aa840f648b026841eec538755240ed
= <=>
2022-05-09 11:51:04 +0300

    adding dbHelper

---
+++ src/main/java/sql/DbHelper.java
@@ -0,0 +1,20 @@
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbHelper {
+private static String dbUrl="jdbc:sqlite:personel_data.db";
+
+    public static Connection getConnection() throws SQLException{
+        return (Connection)DriverManager.getConnection(dbUrl);
+    }
+
+    public static void showError(SQLException e) {
+        System.out.println("Error code "+e.getErrorCode());
+        System.out.println("Error message "+e.getMessage());
+    }
+
+    
+}
