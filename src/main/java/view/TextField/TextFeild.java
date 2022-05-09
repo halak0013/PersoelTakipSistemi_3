fa719e37824217dd6aef8ffc6bfee20c9dba495c
= <=>
2022-05-09 19:38:28 +0300

    adding custom text field

--- src/main/java/view/TextField/TextFeild.java
+++ src/main/java/view/TextField/TextFeild.java
@@ -1,10 +1,16 @@
-
+//Bisimillahirrahmanirrahim
 package view.TextField;
 
-/**
- *
- * @author bismih
- */
-public class TextFeild {
+import java.awt.Color;
+import javax.swing.JTextField;
+import static javax.swing.BorderFactory.createMatteBorder;
+
+public class TextFeild extends JTextField{
+
+    public TextFeild() {
+        setBorder(createMatteBorder(0,0,2,0,new Color(0,0,0)));
+    }
     
+    
+    //setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(0, 0, 0)));
 }
