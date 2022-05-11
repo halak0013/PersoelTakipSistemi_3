3cfa83fb100cdcf4f8612b6251a19ce8d7f4cb4a
= <=>
2022-05-11 16:35:53 +0300

    buton taslağı

--- src/main/java/view/buttons/ButtonN.java
+++ src/main/java/view/buttons/ButtonN.java
@@ -20,36 +20,38 @@ public class ButtonN extends JButton {
     private short radius = 25;
 
     public ButtonN() {
-
+        // !!!!!!!!!! bu çok önemli yoksa buton başta renksiz oluyor
+        setBackground(colorMain);
         setContentAreaFilled(false);
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                //? fare üzerine geldiğinde olan renk
+                // ? fare üzerine geldiğinde olan renk
                 setBackground(Color.orange);
-                over=true;
+                over = true;
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-                //? mousu bıraktıktan sonraki renk
+                // ? mousu bıraktıktan sonraki renk
                 setBackground(Color.pink);
-                over=false;
+                over = false;
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
-                //? basılı tutulduğundaki renk
+                // ? basılı tutulduğundaki renk
                 setBackground(Color.magenta);
             }
+
             @Override
             public void mouseReleased(MouseEvent e) {
-                if(over){
-                    //? basıp bıraktıktan sonra üzerinde durduğu sürece olan renk
-                setBackground(Color.CYAN);
-                }else{
-                    //? fareye basıp eğer üzerinde bırkamzsak olacak renk
-                setBackground(Color.yellow);
+                if (over) {
+                    // ? basıp bıraktıktan sonra üzerinde durduğu sürece olan renk
+                    setBackground(Color.CYAN);
+                } else {
+                    // ? fareye basıp eğer üzerinde bırkamzsak olacak renk
+                    setBackground(Color.yellow);
                 }
             }
 
@@ -60,11 +62,14 @@ public class ButtonN extends JButton {
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        //? kenar rengi
+        // g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+        // RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+        // ? kenar rengi
         g2.setColor(Color.red);
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
         g2.setColor(getBackground());
-        g2.fillRoundRect(2, 2, getWidth()-4, getHeight()-4, radius, radius);
+        g2.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, radius, radius);
         super.paintComponent(g);
     }
 
