d5cf491e3bef807e7db6abc5788a3ae7f51b5da3
= <=>
2022-05-21 22:54:59 +0300

    svgImageViewer improving

--- src/main/java/view/image/SvgImage.java
+++ src/main/java/view/image/SvgImage.java
@@ -2,21 +2,35 @@
 package view.image;
 
 import com.formdev.flatlaf.extras.FlatSVGIcon;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JLabel;
 
 public class SvgImage extends JLabel {
 
     private FlatSVGIcon svg;
-    private String image="close";
+    private String image="search";
+
+    public SvgImage() {
+        super();
+        scale();
+    }
 
     public void scale() {
         try {
+            setPreferredSize(new Dimension(100,100));
             svg = new FlatSVGIcon("resources/icons/"+getImage()+".svg", getWidth(), getHeight());
             setIcon(svg);
         } catch (Exception e) {
             System.out.println("svg error"+e.getMessage());
         }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        scale();
     }
 
     /**
